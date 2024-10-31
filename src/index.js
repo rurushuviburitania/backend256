bdfb45ce21ff847252ffd8cafde5ae73c37faa52
rurushu <chauxsergio@gmail.com>
2024-10-31 01:06:51 -0500

    views

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.set("views", viewspath);
 app.set("view engine", "ejs");
 
 app.get('/', (req,res) => {
-    res.render('index');
+    res.render('index.ejs');
 })
 
 app.get('/logout', (req,res) => {
